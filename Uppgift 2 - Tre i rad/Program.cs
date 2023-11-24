d5095cb41c6b6d9eb1971f519c99c0ab703873aa
hast06001 <hast06001@E-PW024NW1>
2023-11-24 09:54:24 +0100

    Add project files.

---
+++ Uppgift 2 - Tre i rad/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Uppgift_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in värdet på tal N.");
+            int N = int.Parse(Console.ReadLine());
+            int treigaTal = 0;
+            int a = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                a = i * (i + 1) * i * (i + 2);
+                if (a < N)
+                    treigaTal++;
+            }
+            Console.WriteLine($"Svar: Det är {treigaTal} treiga tal som är mindre än {N}.");
+            Console.ReadKey();
+        }
+    }
+}
